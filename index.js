12cff474f7436854cd3f35b02c211847d39f5ae9
Kevin Gray <kevin@amdtel.co.uk>
2020-03-10 12:06:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+export { TemplateMinWebcomponent } from './src/TemplateMinWebcomponent.js';
---
+++ template-min-webcomponent.js
@@ -0,0 +1,3 @@
+import { TemplateMinWebcomponent } from './src/TemplateMinWebcomponent.js';
+
+window.customElements.define('template-min-webcomponent', TemplateMinWebcomponent);
